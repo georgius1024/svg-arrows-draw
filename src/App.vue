37787690f1c93429d26f11bf6413d8273178c1cd
georgius1024 <georgius@ural.ru>
2021-12-09 17:25:50 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <Arrow :fromX="originX" :fromY="originY" :toX="mouseX" :toY="mouseY" :stroke="12"/>
+</template>
+
+<script>
+import Arrow from './components/Arrow.vue'
+export default {
+  components: {
+    Arrow
+  },
+  data() {
+    return {
+      originX: 800,
+      originY: 600,
+      mouseX: 100,
+      mouseY: 100
+    }
+  },
+  mounted() {
+    this.mouseListener = (e) => {
+      this.mouseX = e.pageX
+      this.mouseY = e.pageY
+    }
+    document.addEventListener('mousemove', this.mouseListener)
+  },
+  beforeUnmount() {
+    document.removeEventListener('mousemove', this.mouseListener)
+  }
+};
+</script>
+
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100vh;
+  position: relative;;
+}
+</style>
---
+++ src/components/Arrow.vue
@@ -0,0 +1,93 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width" :height="height"
+    :viewBox="viewBox"
+    :style="style"
+  >
+    <marker
+      id="triangle"
+      viewBox="0 0 10 10"
+      refX="0"
+      refY="5"
+      markerUnits="strokeWidth"
+      markerWidth="4"
+      markerHeight="3"
+      orient="auto"
+    >
+      <path d="M 0 0 L 10 5 L 0 10 z" />
+    </marker>
+    <line :x1="x1" :y1="y1" :x2="x2" :y2="y2" marker-end="url(#triangle)" stroke="black" :stroke-width="stroke"/>
+  </svg>
+</template>
+<script>
+export default {
+  props: ["fromX", "fromY", "toX", "toY", "stroke"],
+  computed: {
+    width() {
+      return Math.max(Math.abs(this.fromX - this.toX), this.stroke * 3);
+    },
+    height() {
+      return Math.max(Math.abs(this.fromY - this.toY), this.stroke * 3);
+    },
+    viewBox() {
+      return `0 0 ${this.width} ${this.height}`;
+    },
+    offset() {
+      return this.stroke * 4;
+    },
+    x1() {
+      return this.fromX < this.toX ? this.offset : this.width - this.offset;
+    },
+    y1() {
+      return this.fromY < this.toY ? this.offset : this.height - this.offset;
+    },
+    x2() {
+      return this.fromX < this.toX ? this.width - this.offset : this.offset;
+    },
+    y2() {
+      return this.fromY < this.toY ? this.height - this.offset : this.offset;
+    },
+    left() {
+      return this.fromX < this.toX ? this.fromX : this.fromX - this.width;
+    },
+    top() {
+      return this.fromY < this.toY ? this.fromY : this.fromY - this.height;
+    },
+    // x1() {
+    //   return this.fromX < this.toX ? this.offset : this.width - this.offset;
+    // },
+    // y1() {
+    //   return this.fromY < this.toY ? this.offset : this.height - this.offset;
+    // },
+    // x2() {
+    //   return this.fromX < this.toX ? this.width - this.offset : this.offset;
+    // },
+    // y2() {
+    //   return this.fromY < this.toY ? this.height - this.offset : this.offset;
+    // },
+
+    // x1() {
+    //   return this.fromX < this.toX ? 10 : this.width - 20;
+    // },
+    // y1() {
+    //   return this.fromY < this.toY ? 10 : this.height - 20;
+    // },
+    // x2() {
+    //   return this.fromX < this.toX ? this.width - 20 : 10;
+    // },
+    // y2() {
+    //   return this.fromY < this.toY ? this.height - 20 : 10;
+    // },
+    style() {
+      return {
+        position: 'absolute',
+        left: `${this.left}px`, // XY needs to be improved for other quadrant
+        top: `${this.top}px`,
+        // left: `${Math.min(this.fromX, this.toX)}px`,
+        // top: `${Math.min(this.fromY, this.toY)}px`,
+      }
+    }
+  },
+};
+</script>
