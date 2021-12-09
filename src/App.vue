6c0d865f85e888aacb1468186b5dbda9b8e83387
georgius1024 <georgius@ural.ru>
2021-12-09 22:41:03 +0500

    drawer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,52 @@
 <template>
-  {{mouseX}}x{{mouseY}}
+  <template v-if="points.length">
+    <template v-for="(point, index) in points">
+      <span
+        :style="{
+          position: 'absolute',
+          left: `${point.x - 2}px`,
+          top: `${point.y - 2}px`,
+          width: '4px',
+          height: '4px',
+          background: 'black',
+          borderRadius: '100%',
+          zIndex: 1000,
+        }"
+      ></span>
+      <Connector
+        v-if="index > 0"
+        :fromX="points[index - 1].x"
+        :fromY="points[index - 1].y"
+        :toX="point.x"
+        :toY="point.y"
+        :stroke="12"
+      />
+    </template>
+    <template v-if="current.x">
+      <Connector
+        :fromX="points[points.length - 1].x"
+        :fromY="points[points.length - 1].y"
+        :toX="current.x"
+        :toY="current.y"
+        :stroke="12"
+      />
+    </template>
+  </template>
+  <template v-else>
+    <div
+      style="
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      "
+    >
+      Click to start path
+    </div>
+  </template>
+  <!-- {{mouseX}}x{{mouseY}}
   <Arrow
     :fromX="originX"
     :fromY="originY"
@@ -30,32 +77,46 @@
       borderRadius: '100%',
       zIndex: 1000
     }"
-  ></span>
+  ></span> -->
 </template>
 
 <script>
-import Arrow from "./components/Arrow.vue";
+import Connector from "./components/Connector.vue";
 export default {
   components: {
-    Arrow,
+    Connector,
   },
   data() {
     return {
-      originX: 600,
-      originY: 400,
-      mouseX: 150,
-      mouseY: 800,
+      points: [],
+      current: {},
     };
   },
   mounted() {
-    this.mouseListener = (e) => {
-      this.mouseX = e.pageX;
-      this.mouseY = e.pageY;
+    this.mouseUpListener = (e) => {
+      this.points.push(this.snapped({ x: e.pageX, y: e.pageY }));
     };
-    document.addEventListener('mousemove', this.mouseListener)
+    this.mouseMoveListener = (e) => {
+      this.current = this.snapped({ x: e.pageX, y: e.pageY });
+    };
+    document.addEventListener("mouseup", this.mouseUpListener);
+    document.addEventListener("mousemove", this.mouseMoveListener);
   },
   beforeUnmount() {
-    document.removeEventListener("mousemove", this.mouseListener);
+    document.removeEventListener("mouseup", this.mouseUpListener);
+    document.removeEventListener("mousemove", this.mouseMoveListener);
+  },
+  methods: {
+    snapToGrid(value, step = 50) {
+      return Math.round(value / step) * step;
+    },
+    snapped(point) {
+      const { x, y } = point;
+      return {
+        x: this.snapToGrid(x),
+        y: this.snapToGrid(y),
+      };
+    },
   },
 };
 </script>
--- src/components/Connector.vue
+++ src/components/Connector.vue
@@ -1,6 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    class="connector"
     :width="width"
     :height="height"
     :viewBox="viewBox"
